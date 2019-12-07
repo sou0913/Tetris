88384d88cba4ef02a09a04441aa020d29fdf9cde
sou0913 <usokisasa0913@gmail.com>
2019-12-07 19:16:18 +0900

    [Add]block fall and pile up

--- main.java
+++ main.java
@@ -2,19 +2,25 @@ public class main {
     public static void main(String args[]) {
         //最初
         Map map = new Map();
-        Block block = new Block();
         map.initialize();
-        // 落下動作
-        for(int i=0;i<5;i++) {
-            map.isErased(block.getRow(),block.getColumn());
-            map.isNoted(block.getRow(),block.getColumn());
-            map.show();
-            if (map.check_below(block.getRow(),block.getColumn())) {
-                block.fall();
-            } else {break;}
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){};
+        // boolean game = true;
+        int game = 0;
+        while (game < 5) {
+            // ブロックを生成
+            Block block = new Block();
+            // 落下動作
+            for(int i=0;i<5;i++) {
+                map.isErased(block.getRow(),block.getColumn());
+                map.isNoted(block.getRow(),block.getColumn());
+                map.show();
+                if (map.check_below(block.getRow(),block.getColumn())) {
+                    block.fall();
+                } else {break;}
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){}
+            }
+            game += 1;
         }
     }
 }
